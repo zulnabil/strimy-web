883c2f4edba3515356e60bf88058bc4c929eb39e
zulnabil <zulnabil.22@gmail.com>
2025-02-11 02:04:10 +0700

    feat(theme): color vars

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import "~/styles/globals.scss";
 import SideBar from "~/app/common/components/layouts/SideBar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="theme-dark">
         <SideBar />
         {children}
       </body>
